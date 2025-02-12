ab84d699984810dc6645b836f1776298f65a6c71
Ximo Casanova <ximo.casanova@palluc.com>
2025-02-12 13:23:08 +0100

    edita codigo completo

--- components/steps.js
+++ components/steps.js
@@ -15,143 +15,26 @@ const Steps = (props) => {
                 fet durant l&apos;any.
               </p>
             </div>
-            {/* Añadiendo el paso 1 */}
-            <div className="steps-container3">
-              <div className="steps-containerA thq-card">
-                <h2 className="thq-heading-2">{props.step1Title}</h2>
-                <img
-                  alt={props.imageAlt1}
-                  src={props.imageSrc1}
-                  className="steps-image1"
-                />
-                <img
-                  alt={props.imageAlt2}
-                  src={props.imageSrc2}
-                  className="steps-image2"
-                />
+            {[
+              { title: props.step1Title, imgAlt1: props.imageAlt1, imgSrc1: props.imageSrc1, imgAlt2: props.imageAlt2, imgSrc2: props.imageSrc2, description: props.step1Description, stepNumber: "01" },
+              { title: props.step2Title, imgAlt1: props.imageAlt3, imgSrc1: props.imageSrc3, imgAlt2: props.imageAlt4, imgSrc2: props.imageSrc4, description: props.step2Description, stepNumber: "02" },
+              { title: props.step3Title, imgAlt1: props.imageAlt5, imgSrc1: props.imageSrc5, imgAlt2: props.imageAlt6, imgSrc2: props.imageSrc6, description: props.step3Description, stepNumber: "03" },
+              { title: props.step4Title, imgAlt1: props.imageAlt7, imgSrc1: props.imageSrc7, description: props.step4Description, additionalDescription: props.step4AditionalDescription, stepNumber: "04" },
+              { title: props.step5Title, imgAlt1: props.imageAlt9, imgSrc1: props.imageSrc9, description: props.step5Description, additionalDescription: props.step5AditionalDescription, stepNumber: "05" },
+              { title: props.step6Title, imgAlt1: props.imageAlt10, imgSrc1: props.imageSrc10, imgAlt2: props.imageAlt11, imgSrc2: props.imageSrc11, description: props.step6Description, additionalDescription: props.step6AditionalDescription, stepNumber: "06" },
+              { title: props.step7Title, imgAlt1: props.imageAlt13, imgSrc1: props.imageSrc13, imgAlt2: props.imageAlt14, imgSrc2: props.imageSrc14, description: props.step7Description, additionalDescription: props.step7AditionalDescription, stepNumber: "07" },
+            ].map((step, index) => (
+              <div key={index} className={`steps-container${String.fromCharCode(65 + index)} thq-card`}>
+                <h2 className="thq-heading-2">{step.title}</h2>
+                <label className="steps-text16 thq-heading-3">{step.stepNumber}</label>
+                <img alt={step.imgAlt1} src={step.imgSrc1} className="steps-image1" />
+                {step.imgAlt2 && <img alt={step.imgAlt2} src={step.imgSrc2} className="steps-image2" />}
                 <span className="steps-text13 thq-body-small">
-                  {props.step1Description}
-                </span>
-                <label className="steps-text14 thq-heading-3">01</label>
-              </div>
-              {/* Añadiendo el paso 2 */}
-              <div className="steps-containerB thq-card">
-                <h2 className="thq-heading-2">{props.step2Title}</h2>
-                <label className="steps-text16 thq-heading-3">02</label>
-                <img
-                  alt={props.imageAlt3}
-                  src={props.imageSrc3}
-                  className="steps-image1"
-                />
-                <img
-                  alt={props.imageAlt4}
-                  src={props.imageSrc4}
-                  className="steps-image2"
-                />
-                <span className="steps-text17 thq-body-small">
-                  {props.step2Description}
+                  {step.description}
+                  {step.additionalDescription && <><br /> <br />{step.additionalDescription}</>}
                 </span>
               </div>
-              {/* Añadiendo el paso 3 */}
-              <div className="steps-containerC thq-card">
-                <h2 className="thq-heading-2">{props.step3Title}</h2>
-                <img
-                  alt={props.imageAlt5}
-                  src={props.imageSrc5}
-                  className="steps-image1"
-                />
-                <img
-                  alt={props.imageAlt6}
-                  src={props.imageSrc6}
-                  className="steps-image2"
-                />
-                <span className="steps-text13 thq-body-small">
-                  {props.step3Description}
-                </span>
-                <label className="steps-text14 thq-heading-3">03</label>
-              </div>
-              {/* Añadiendo el paso 4 */}
-              <div className="steps-containerD thq-card">
-                <h2 className="thq-heading-2">{props.step4Title}</h2>
-                <label className="steps-text16 thq-heading-3">04</label>
-                <img
-                  alt={props.imageAlt7}
-                  src={props.imageSrc7}
-                  className="steps-image1"
-                />
-                <img
-                  alt={props.imageAlt8}
-                  src={props.imageSrc8}
-                  className="steps-image2"
-                />
-                <span className="steps-text17 thq-body-small">
-                  {props.step4Description}
-                  <br /> <br />
-                  {props.step4AditionalDescription}
-                </span>
-              </div>
-              {/* Añadiendo el paso 5 */}
-              <div className="steps-containerA thq-card">
-                <h2 className="thq-heading-2">{props.step5Title}</h2>
-                <img
-                  alt={props.imageAlt9}
-                  src={props.imageSrc9}
-                  className="steps-image1"
-                />
-                <img
-                  alt={props.imageAlt10}
-                  src={props.imageSrc10}
-                  className="steps-image2"
-                />
-                <span className="steps-text13 thq-body-small">
-                  {props.step5Description}
-                  <br /> <br />
-                  {props.step5AditionalDescription}
-                </span>
-                <label className="steps-text14 thq-heading-3">05</label>
-              </div>
-              {/* Añadiendo el paso 6 */}
-              <div className="steps-containerB thq-card">
-                <h2 className="thq-heading-2">{props.step6Title}</h2>
-                <label className="steps-text16 thq-heading-3">06</label>
-                <img
-                  alt={props.imageAlt11}
-                  src={props.imageSrc11}
-                  className="steps-image1"
-                />
-                <img
-                  alt={props.imageAlt12}
-                  src={props.imageSrc12}
-                  className="steps-image2"
-                />
-                <span className="steps-text13 thq-body-small">
-                  {props.step6Description}
-                  <br /> <br />
-                  {props.step6AditionalDescription}
-                </span>
-              </div>
-              {/* Añadiendo el paso 7 */}
-              <div className="steps-containerC thq-card">
-                <h2 className="thq-heading-2">{props.step7Title}</h2>
-                <label className="steps-text16 thq-heading-3">07</label>
-                <img
-                  alt={props.imageAlt13}
-                  src={props.imageSrc13}
-                  className="steps-image1"
-                />
-                <img
-                  alt={props.imageAlt14}
-                  src={props.imageSrc14}
-                  className="steps-image2"
-                />
-                <span className="steps-text13 thq-body-small">
-                  {props.step7Description}
-                  <br /> <br />
-                  {props.step7AditionalDescription}
-                </span>
-              </div>
-
-            </div>
+            ))}
           </div>
         </div>
       </div>
@@ -184,16 +67,6 @@ const Steps = (props) => {
             align-items: flex-start;
             flex-direction: column;
           }
-          .steps-container3 {
-            grid-area: span 1 / span 1 / span 1 / span 1;
-          }
-          .steps-container4 {
-            top: 10%;
-            position: sticky;
-            transform: rotate(-2deg);
-            margin-bottom: var(--dl-space-space-fourunits);
-            background-color: var(--dl-color-theme-accent1);
-          }
           .steps-image1 {
             width: 170px;
             height: 210px;
@@ -220,42 +93,18 @@ const Steps = (props) => {
             right: var(--dl-space-space-unit);
             position: absolute;
             font-size: 40px;
-            font-style: normal;
             font-weight: 700;
           }
-          .steps-container5 {
-            top: 10%;
-            position: sticky;
-            transform: rotate(2deg);
-            margin-bottom: var(--dl-space-space-twounits);
-            background-color: var(--dl-color-theme-accent2);
-          }
           .steps-containerA {
-            top: 10%;
-            position: sticky;
-            transform: rotate(2deg);
-            margin-bottom: var(--dl-space-space-twounits);
             background-color: var(--dl-color-theme-accent1);
           }
           .steps-containerB {
-            top: 10%;
-            position: sticky;
-            transform: rotate(-2deg);
-            margin-bottom: var(--dl-space-space-twounits);
             background-color: var(--dl-color-theme-accent2);
           }
           .steps-containerC {
-            top: 10%;
-            position: sticky;
-            transform: rotate(2deg);
-            margin-bottom: var(--dl-space-space-twounits);
-            background-color:  #f9d35c;
+            background-color: #f9d35c;
           }
           .steps-containerD {
-            top: 10%;
-            position: sticky;
-            transform: rotate(-2deg);
-            margin-bottom: var(--dl-space-space-twounits);
             background-color: #ff8f8f;
           }
           .steps-text16 {
@@ -263,64 +112,14 @@ const Steps = (props) => {
             right: var(--dl-space-space-unit);
             position: absolute;
             font-size: 40px;
-            font-style: normal;
             font-weight: 700;
-          }
-          .steps-image3 {
-            width: 170px;
-            height: 210px;
-            align-self: flex-start;
-            margin-top: 10%;
-            object-fit: cover;
-            margin-left: 5%;
-            margin-bottom: 10%;
-          }
-          .steps-image4 {
-            width: 178px;
-            height: 219px;
-            align-self: flex-end;
-            margin-top: -50%;
-            object-fit: cover;
-            margin-right: 10%;
           }
           .steps-text17 {
             margin-top: 0%;
             text-align: center;
           }
-          .steps-container6 {
-            top: 10%;
-            position: sticky;
-            transform: rotate(-2deg);
-            margin-bottom: var(--dl-space-space-twounits);
-            background-color: var(--dl-color-theme-accent1);
-          }
-          .steps-text19 {
-            text-align: center;
-          }
-          .steps-text20 {
-            top: var(--dl-space-space-unit);
-            right: var(--dl-space-space-unit);
-            position: absolute;
-            font-size: 40px;
-            font-style: normal;
-            font-weight: 700;
-          }
-          .steps-container7 {
-            top: 10%;
-            position: sticky;
-            transform: rotate(2deg);
-            background-color: var(--dl-color-theme-accent2);
-          }
-          .steps-text22 {
-            text-align: center;
-          }
-          .steps-text23 {
-            top: var(--dl-space-space-unit);
-            right: var(--dl-space-space-unit);
-            position: absolute;
-            font-size: 40px;
-            font-style: normal;
-            font-weight: 700;
+          .steps-container6, .steps-container7 {
+            width: 100%;
           }
           @media (max-width: 991px) {
             .steps-max-width {
@@ -332,53 +131,40 @@ const Steps = (props) => {
               position: static;
               margin-bottom: var(--dl-space-space-twounits);
             }
-            .steps-container4 {
-              width: 100%;
-            }
-            .steps-container5 {
-              width: 100%;
-            }
-            .steps-container6 {
-              width: 100%;
-            }
-            .steps-container7 {
-              width: 100%;
-            }
           }
         `}
       </style>
     </>
-  )
-}
+  );
+};
 
 Steps.defaultProps = {
-
   step7Title: "Rifa extraordinària",
   step7Description: "Guanyador d'un Iphone 16 amb el número 6993",
   step7AditionalDescription: "No premiat. Revisa les teues papeletes i fica't en contacte amb els i les festeres",
-  imageSrc13: '/rifes/numeros/num_guanyador_21desembre.jpg',
-  imageSrc14: '/rifes/premiats/guanyador_21desembre.jpg',
+  imageSrc15: '/rifes/numeros/num_guanyador_iphone.jpg',
+  imageSrc16: '/rifes/premiats/guanyador_iphone.jpg',
 
   step6Title: "Divendres de mes de febrer",
   step6Description: "Guanyador d'un sopar a la pizzeria Da Vito amb el número 0974",
   step6AditionalDescription: "No premiat. Revisa les teues papeletes i fica't en contacte amb els i les festeres",
-  imageSrc9: '/rifes/numeros/num_guanyador_febrer.jpg',
-  imageSrc10: '/rifes/premiats/guanyador_febrer.jpg',
+  imageSrc13: '/rifes/numeros/num_guanyador_febrer.jpg',
+  imageSrc14: '/rifes/premiats/guanyador_febrer.jpg',
 
   step5Title: "Divendres de mes de gener",
   step5Description: "Guanyador d'un sopar a la pizzeria Da Vito amb el número 6154",
   step5AditionalDescription: "No premiat. Revisa les teues papeletes i fica't en contacte amb els i les festeres",
-  imageSrc8: '/rifes/numeros/num_guanyador_gener.jpg',
+  imageSrc12: '/rifes/numeros/num_guanyador_gener.jpg',
 
   step6Title: "21 de desembre",
   step6Description: "Guanyadora d'un pernil amb el número 9849",
-  imageSrc9: '/rifes/numeros/num_guanyador_21desembre.jpg',
-  imageSrc10: '/rifes/premiats/guanyador_21desembre.jpg',
+  imageSrc10: '/rifes/numeros/num_guanyador_21desembre.jpg',
+  imageSrc11: '/rifes/premiats/guanyador_21desembre.jpg',
 
   step5Title: "Divendres de mes de desembre",
   step5Description: "Premi d'un esmorzar a Ca Blau al número 0639",
   step5AditionalDescription: "No premiat. Revisa les teues papeletes i fica't en contacte amb els i les festeres",
-  imageSrc8: '/rifes/numeros/num_guanyador_desembre.jpg',
+  imageSrc9: '/rifes/numeros/num_guanyador_desembre.jpg',
 
   step4Title: "Divendres de mes d'octubre",
   step4Description: "Premi d'un esmorzar a Ca Blau al número 7864",
@@ -401,61 +187,43 @@ Steps.defaultProps = {
   imageSrc2: '/rifes/premiats/guanyador_15A.jpeg',
 };
 
-
 Steps.propTypes = {
-  step1Title: PropTypes.string,
-  step1Description: PropTypes.string,
-  imageSrc1: PropTypes.string,
-  imageAlt1: PropTypes.string,
-  imageSrc2: PropTypes.string,
-  imageAlt2: PropTypes.string,
+  step7Title: PropTypes.string,
+  step7Description: PropTypes.string,
+  step7AditionalDescription: PropTypes.string,
+  imageSrc15: PropTypes.string,
+  imageSrc16: PropTypes.string,
 
-  step2Title: PropTypes.string,
-  step2Description: PropTypes.string,
-  imageSrc3: PropTypes.string,
-  imageAlt3: PropTypes.string,
-  imageSrc4: PropTypes.string,
-  imageAlt4: PropTypes.string,
+  step6Title: PropTypes.string,
+  step6Description: PropTypes.string,
+  step6AditionalDescription: PropTypes.string,
+  imageSrc13: PropTypes.string,
+  imageSrc14: PropTypes.string,
 
-  step3Title: PropTypes.string,
-  step3Description: PropTypes.string,
-  imageSrc5: PropTypes.string,
-  imageAlt5: PropTypes.string,
-  imageSrc6: PropTypes.string,
-  imageAlt6: PropTypes.string,
+  step5Title: PropTypes.string,
+  step5Description: PropTypes.string,
+  step5AditionalDescription: PropTypes.string,
+  imageSrc12: PropTypes.string,
 
   step4Title: PropTypes.string,
   step4Description: PropTypes.string,
   step4AditionalDescription: PropTypes.string,
   imageSrc7: PropTypes.string,
-  imageAlt7: PropTypes.string,
-  imageSrc8: PropTypes.string,
-  imageAlt8: PropTypes.string,
 
-  step5Title: PropTypes.string,
-  step5Description: PropTypes.string,
-  step5AditionalDescription: PropTypes.string,
-  imageSrc9: PropTypes.string,
-  imageAlt9: PropTypes.string,
-  imageSrc10: PropTypes.string,
-  imageAlt10: PropTypes.string,
+  step3Title: PropTypes.string,
+  step3Description: PropTypes.string,
+  imageSrc5: PropTypes.string,
+  imageSrc6: PropTypes.string,
 
-  step6Title: PropTypes.string,
-  step6Description: PropTypes.string,
-  step6AditionalDescription: PropTypes.string,
-  imageSrc11: PropTypes.string,
-  imageAlt11: PropTypes.string,
-  imageSrc12: PropTypes.string,
-  imageAlt12: PropTypes.string,
+  step2Title: PropTypes.string,
+  step2Description: PropTypes.string,
+  imageSrc3: PropTypes.string,
+  imageSrc4: PropTypes.string,
 
-  step7Title: PropTypes.string,
-  step7Description: PropTypes.string,
-  step7AditionalDescription: PropTypes.string,
-  imageSrc13: PropTypes.string,
-  imageAlt13: PropTypes.string,
-  imageSrc14: PropTypes.string,
-  imageAlt14: PropTypes.string,
-
+  step1Title: PropTypes.string,
+  step1Description: PropTypes.string,
+  imageSrc1: PropTypes.string,
+  imageSrc2: PropTypes.string,
 };
 
 
